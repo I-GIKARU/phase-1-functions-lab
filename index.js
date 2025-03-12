2a0f3872a4db199165c6617cf3ad3fb17b0ca432
I-GIKARU <aizohgikaru@gmail.com>
2025-03-12 21:39:47 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+// Define HQ location
+const headquarters = 42;
+
+// Function to calculate distance from HQ in blocks
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - headquarters);
+}
+
+// Function to calculate distance from HQ in feet
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+// Function to calculate distance traveled in feet
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264;
+}
+
+// Function to calculate fare price
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+
+
